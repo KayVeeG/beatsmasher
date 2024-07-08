18c10f5c07f3a33990b989335f2430e75d292d54
Karl <karl.vongagern@gmail.com>
2024-07-08 17:10:44 +0200

    add buttons for testing and update platformio.ini

--- src/circuitRuntime.cpp
+++ src/circuitRuntime.cpp
@@ -17,6 +17,13 @@ class CircuitRuntime : public smash::SlaveRuntime
             SlaveRuntime::initialize();
             SlaveRuntime::addSpeaker(std::make_shared<smash::Speaker>(0, 12));
             SlaveRuntime::addButton(smash::SlaveButton(0, 36));
+            SlaveRuntime::addButton(smash::SlaveButton(1, 39));
+            SlaveRuntime::addButton(smash::SlaveButton(2, 34));
+            SlaveRuntime::addButton(smash::SlaveButton(3, 35));
+            SlaveRuntime::addButton(smash::SlaveButton(4, 32));
+            SlaveRuntime::addButton(smash::SlaveButton(5, 33));
+            SlaveRuntime::addButton(smash::SlaveButton(6, 25));
+            SlaveRuntime::addButton(smash::SlaveButton(7, 26));
         }
         void update() override
         {
